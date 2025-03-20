2a2d409347ef755c0ce1286d188b50f41cdc5387
RaahimAslam <160450802+RaahimAslam@users.noreply.github.com>
2025-03-20 23:43:42 +0530

    Removed unused code lines

--- FrontEnd/src/Regulations/TopBar.jsx
+++ FrontEnd/src/Regulations/TopBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./TopBar.module.css";
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
 
 function TopBar() {
-    const navigate = useNavigate();
+//     const navigate = useNavigate();
     return (
         <header className={styles.topBar}>
             <img src="/assets/profileIcon.png" alt="Regulations icon" className={styles.img3}/>
