b011daf6530c1f112e74d45370c84728613d97da
Neovin-Peiris <neovinl.peiris@gmail.com>
2025-03-21 13:06:50 +0530

    Renamed option

--- FrontEnd/src/About/OptionSection.jsx
+++ FrontEnd/src/About/OptionSection.jsx
@@ -27,7 +27,7 @@ const OptionsSection = () => {
       <div className={styles.optionsContainer}>
         <OptionCard
           backgroundSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/af1fe817ef52760e2cbc21c532d3a62c7168d7cf60d708dd3475fb6faf02666f?placeholderIfAbsent=true&apiKey=961e21a64e344f339dc230b663f13664"
-          text="FILTERED"
+          text="FILTERED REGULATIONS"
         />
         <OptionCard
           backgroundSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/f277339d4022d4ce349eaccadcf339da6e120d32769d83cf7d448f28c8ef19cf?placeholderIfAbsent=true&apiKey=961e21a64e344f339dc230b663f13664"
