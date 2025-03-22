7bceb1341b4fe9e3986402d7550fffaec016c8f0
Maariya Marzook <maariya.marzook@gmail.com>
2025-03-22 22:37:17 +0530

    Minor update to About page hero section

--- FrontEnd/src/About/Hero.jsx
+++ FrontEnd/src/About/Hero.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
       <img
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/04c9068d4296d6c8fa07d5ea1c10db7b722abb4077df9e121a45391e04ff62d1?placeholderIfAbsent=true&apiKey=961e21a64e344f339dc230b663f13664"
+        src="/assets/home.PNG"
         alt="Background"
         className={styles.heroBackground}
       />
